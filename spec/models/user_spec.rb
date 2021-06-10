d1d7593b951de19433643795b7ed9f0f5f75a052
hideki1104 <hideki11047@gmail.com>
2021-06-11 07:51:57 +0900

    Userモデルのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   it '名前、メール、パスワードがある場合、有効である' do
     user = User.new(
       name: "example",
@@ -11,10 +12,50 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it '名前がない場合、無効である'
-  it 'メールアドレスがない場合、無効である'
-  it 'メールアドレスが正しい形式でない場合、無効である'
-  it '重複したメールアドレスの場合、無効である'
-  it 'パスワードがない場合、無効である'
-  it 'パスワードと確認用パスワードが異なる場合、無効である'
+  it 'メールアドレスがない場合、無効である' do
+    user = User.new(
+      name: "example",
+      email: nil,
+      password: "password",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'メールアドレスが正しい形式でない場合、無効である' do
+    user = User.new(
+      name: "example",
+      email: "testexample",
+      password: "password",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("is not an email")
+  end
+
+  it '重複したメールアドレスの場合、無効である' do
+    user = User.new(
+      name: "example",
+      email: "test@example.com",
+      password: "password",
+    )
+    user.save
+
+    user = User.new(
+      name: "example",
+      email: "test@example.com",
+      password: "password",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it 'パスワードがない場合、無効である' do
+    user = User.new(
+      name: "example",
+      email: "test@example.com",
+      password: nil,
+    )
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
 end
