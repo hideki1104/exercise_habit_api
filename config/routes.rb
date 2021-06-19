eb663022eda0250ecc466bbcb33498ec9c08be21
hideki1104 <hideki11047@gmail.com>
2021-06-19 13:34:34 +0900

    管理者側ログイン画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'user/auth/', controllers: {
-          registrations: 'api/v1/auth/registrations',
+          registrations: 'api/v1/user/auth/registrations',
           sessions: 'api/v1/user/auth/sessions'
       }
       mount_devise_token_auth_for 'Admin', at: 'admin/auth/', controllers: {
