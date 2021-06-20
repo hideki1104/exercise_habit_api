d9d14715c186c3cfb91f883272b16c9b61437254
hideki1104 <hideki11047@gmail.com>
2021-06-20 13:26:37 +0900

    Userモデルの誤字を修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it '名前、メール、パスワードがある場合、有効である' do
+  it '名前、メールアドレス、パスワードがある場合、有効である' do
     user = User.new(
       name: "example",
       email: "test@example.com",
