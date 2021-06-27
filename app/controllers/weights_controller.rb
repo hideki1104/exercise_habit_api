a39e2bec8565a2cf34615179e7d6926a26fe7a16
hideki1104 <hideki11047@gmail.com>
2021-06-27 13:05:06 +0900

    体重コントローラのcreateアクションの作成

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,2 +1,14 @@
 class WeightsController < ApplicationController
+  def create
+    @user = User.new(weight_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  def weight_params
+    params.permit(:weight, :user_id)
+  end
 end
