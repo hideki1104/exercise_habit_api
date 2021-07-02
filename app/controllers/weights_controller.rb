cae69025facaec4720f3409ca97aeec97862c3d4
hideki1104 <hideki11047@gmail.com>
2021-07-02 16:45:27 +0900

    ユーザーに紐づく最新の体重を取得する処理を追加

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,4 +1,6 @@
 class WeightsController < ApplicationController
+  before_action :authenticate_api_user!
+
   def create
     @weight = Weight.new(weight_params)
     if @weight.save
@@ -6,6 +8,11 @@ class WeightsController < ApplicationController
     else
       render json: @weight.errors
     end
+  end
+
+  def show
+    @weight = current_api_user.weights.order(updated_at: :desc).limit(1)
+    render json: @weight
   end
 
   def weight_params
