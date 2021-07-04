8353362b3d26c69ed03e6a5762bd05a261c13686
hideki1104 <hideki11047@gmail.com>
2021-07-04 16:07:21 +0900

    ユーザーモデルテストのリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,61 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = build(:user)
+  end
 
   it '名前、メールアドレス、パスワードがある場合、有効である' do
-    user = User.new(
-      name: "example",
-      email: "test@example.com",
-      password: "password",
-    )
 
-    expect(user).to be_valid
+    expect(@user).to be_valid
   end
 
   it 'メールアドレスがない場合、無効である' do
-    user = User.new(
-      name: "example",
-      email: nil,
-      password: "password",
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
+    @user.email = nil
+    @user.valid?
+    expect(@user.errors[:email]).to include("can't be blank")
   end
 
   it 'メールアドレスが正しい形式でない場合、無効である' do
-    user = User.new(
-      name: "example",
-      email: "testexample",
-      password: "password",
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("is not an email")
+    @user.email = "testexample"
+    @user.valid?
+    expect(@user.errors[:email]).to include("is not an email")
   end
 
   it '重複したメールアドレスの場合、無効である' do
-    user = User.new(
-      name: "example",
-      email: "test@example.com",
-      password: "password",
-    )
-    user.save
-
-    user = User.new(
-      name: "example",
-      email: "test@example.com",
-      password: "password",
-    )
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
+    user       = create(:user)
+    after_user = build(:user)
+    after_user.email = user.email
+    after_user.valid?
+    expect(after_user.errors[:email]).to include("has already been taken")
   end
 
   it 'パスワードがない場合、無効である' do
-    user = User.new(
-      name: "example",
-      email: "test@example.com",
-      password: nil,
-    )
-    user.valid?
-    expect(user.errors[:password]).to include("can't be blank")
+    @user.password = nil
+    @user.valid?
+    expect(@user.errors[:password]).to include("can't be blank")
   end
 end
