62479e35b6ed48d017a4dfb8fd5aa488281a75cc
hideki1104 <hideki11047@gmail.com>
2021-07-04 17:31:39 +0900

    ユーザー更新APIの正常系のテスト追加

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "ユーザー更新機能" do
+    context "正常：" do
+      before do
+        params = {email: "test@example.com", password: "password", confirmation_password: "password"}
+        post api_user_registration_path, params: params
+        response_data = JSON.parse(response.body)
+
+        params = {name: "test", email: "test@example.com", height: 170, sex: 0, birthday: "1990-01-01", training_type: 0}
+        patch "/users/#{response_data['data']['id']}", params: params
+      end
+
+      it 'レスポンスが200で返却されること' do
+        expect(response.status).to eq(200)
+      end
+
+      it '名前の値が正しく更新されていること' do
+        response_data = JSON.parse(response.body)
+        expect(response_data['name']).to eq("test")
+      end
+
+      it 'メールアドレスの値が正しく更新されていること' do
+        response_data = JSON.parse(response.body)
+        expect(response_data['email']).to eq("test@example.com")
+      end
+
+      it '身長の値が正しく更新されていること' do
+        response_data = JSON.parse(response.body)
+        expect(response_data['height']).to eq(170)
+      end
+
+      it '性別の値が正しく更新されていること' do
+        response_data = JSON.parse(response.body)
+        expect(response_data['sex']).to eq(0)
+      end
+
+      it '生年月日の値が正しく更新されていること' do
+        response_data = JSON.parse(response.body)
+        expect(response_data['birthday']).to eq('1990-01-01')
+      end
+
+      it 'トレーニングタイプの値が正しく更新されていること' do
+        response_data = JSON.parse(response.body)
+        expect(response_data['training_type']).to eq(0)
+      end
+    end
+
+    context "異常" do
+    end
+  end
+
+  describe "ユーザー取得機能" do
+    context "正常：" do
+      before do
+        params = {email: "test@example.com", password: "password", confirmation_password: "password"}
+        post api_user_registration_path, params: params
+      end
+    end
+
+    context "異常" do
+    end
   end
 end
