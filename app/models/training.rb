7ee6f49d009e45f488f6116f69709101c9fbf20a
hideki1104 <hideki11047@gmail.com>
2021-08-21 16:34:45 +0900

    urlのバリデーションを11文字に変更

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,6 +1,6 @@
 class Training < ApplicationRecord
   validates :name, presence: true
-  validates :url, {presence: true, length: {minimum: 10, maximum: 10}}
+  validates :url, {presence: true, length: {minimum: 11, maximum: 11}}
   validates :training_type, {presence: true, numericality: true}
   validates :difficuly_type, {presence: true, numericality: true}
   validates :genre_id, {presence: true, numericality: true}
