f5e9bb31e809a332effd0b2f982403648dc3cb91
hideki1104 <hideki11047@gmail.com>
2021-10-16 21:28:43 +0900

    ユーザー編集に自己紹介の項目を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    render json: @user
+    user = User.find(params[:id])
+    render json: user
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      render json: @user
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
     else
-      render json: @user.errors
+      render json: user.errors
     end
   end
 
